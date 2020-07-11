76c1b7fe4dd5fe0672e11b5529a450968856c60f
yoshithKotla <50994281+yoshith@users.noreply.github.com>
2020-07-11 23:18:43 +0530

    Add files via upload

---
+++ chat_bot.py
@@ -0,0 +1,339 @@
+print('*' *140)
+print(""" Hello, Welcome to ZWIGGY Customer care,
+ Since you are new lemme walk you through the steps,
+ Our services will be displayed and all you need to do is sitback and just guide us by entering 
+ numbers corresponding to our services """)
+print('*' *140)
+
+
+name = input("please enter your name: ")
+A_name = "anderson"
+A_name_c = "Anderson"
+B_name = "byron"
+B_name_c = "Byron"
+C_name = "christie"
+C_name_c = "Christie"
+D_name = "dhoni"
+D_name_c = "Dhoni"
+E_name = "ellis"
+E_name_c = "Ellis"
+F_name = "francheska"
+F_name_c = "Francheska"
+G_name = "gunther"
+G_name_c = "Gunther"
+H_name = "haversham"
+H_name_c = "Haversham"
+I_name = "iyaan"
+I_name_c = "Iyaan"
+J_name = "julia"
+J_name_c = "Julia"
+K_name = "kate"
+K_name_c = "Kate"
+L_name = "lucy"
+L_name_c = "Lucy"
+M_name = "mahendra"
+M_name_c = "Mahendra"
+N_name = "nick"
+N_name_c = "Nick"
+O_name = "olivia"
+O_name_c = "Olivia"
+P_name = "pollyana"
+P_name_c = "Pollyana"
+Q_name = "qaira"
+Q_name_c = "Qaira"
+R_name = "rony"
+R_name_c = "Rony"
+S_name = "simba"
+S_name_c = "Simba"
+T_name = "tim"
+T_name_c = "Tim"
+U_name = "urmila"
+U_name_c = "Urmila"
+V_name = "victor"
+V_name_c = "Victor"
+W_name = "winden"
+W_name_c = "Winden"
+X_name = "xanax"
+X_name_c = "Xanax"
+Y_name = "yoshinaka"
+Y_name_c = "Yoshinaka"
+Z_name = "zendaya"
+Z_name_c = "Zendaya"
+
+if name[0] == A_name[0]:
+    print(" wow seriously, you are {0}. Nice to meet you, My name's {1}. Our both names start with 'a' lmao!! ".format(
+        name, A_name_c))
+
+elif name[0] == B_name[0]:
+    print(" wow seriously, you are {0}. Nice to meet you, My name's {1}. Our both names start with 'b' lmao!! ".format(
+        name, B_name_c))
+
+elif name[0] == C_name[0]:
+    print(" wow seriously, you are {0}. Nice to meet you, My name's {1}. Our both names start with 'c' lmao!! ".format(name,C_name_c))
+
+elif name[0] == D_name[0]:
+    print(" wow seriously, you are {0}. Nice to meet you, My name's {1}. Our both names start with 'd' lmao!! ".format(name,D_name_c))
+
+elif name[0] == E_name[0]:
+    print(" wow seriously, you are {0}. Nice to meet you, My name's {1}. Our both names start with 'e' lmao!! ".format(name,E_name_c))
+
+elif name[0] == F_name[0]:
+    print(" wow seriously, you are {0}. Nice to meet you, My name's {1}. Our both names start with 'f' lmao!! ".format(name,F_name_c))
+
+elif name[0] == G_name[0]:
+    print(" wow seriously, you are {0}. Nice to meet you, My name's {1}. Our both names start with 'g' lmao!! ".format(name,G_name_c))
+
+elif name[0] == H_name[0]:
+    print(" wow seriously, you are {0}. Nice to meet you, My name's {1}. Our both names start with 'h' lmao!! ".format(name,H_name_c))
+
+elif name[0] == I_name[0]:
+    print(" wow seriously, you are {0}. Nice to meet you, My name's {1}. Our both names start with 'i' lmao!! ".format(name,I_name_c))
+
+elif name[0] == J_name[0]:
+    print(" wow seriously, you are {0}. Nice to meet you, My name's {1}. Our both names start with 'j' lmao!! ".format(name,J_name_c))
+
+elif name[0] == K_name[0]:
+    print(" wow seriously, you are {0}. Nice to meet you, My name's {1}. Our both names start with 'k' lmao!! ".format(name,K_name_c))
+
+elif name[0] == L_name[0]:
+    print(" wow seriously, you are {0}. Nice to meet you, My name's {1}. Our both names start with 'l' lmao!! ".format(name,L_name_c))
+
+elif name[0] == M_name[0]:
+    print(" wow seriously, you are {0}. Nice to meet you, My name's {1}. Our both names start with 'm' lmao!! ".format(name,M_name_c))
+
+elif name[0] == N_name[0]:
+    print(" wow seriously, you are {0}. Nice to meet you, My name's {1}. Our both names start with 'n' lmao!! ".format(name,N_name_c))
+
+elif name[0] == O_name[0]:
+    print(" wow seriously, you are {0}. Nice to meet you, My name's {1}. Our both names start with 'o' lmao!! ".format(name,O_name_c))
+
+elif name[0] == P_name[0]:
+    print(" wow seriously, you are {0}. Nice to meet you, My name's {1}. Our both names start with 'p' lmao!! ".format(name,P_name_c))
+
+elif name[0] == Q_name[0]:
+    print(" wow seriously, you are {0}. Nice to meet you, My name's {1}. Our both names start with 'q' lmao!! ".format(name,Q_name_c))
+
+elif name[0] == R_name[0]:
+    print(" wow seriously, you are {0}. Nice to meet you, My name's {1}. Our both names start with 'r' lmao!! ".format(name,R_name_c))
+
+elif name[0] == S_name[0]:
+    print(" wow seriously, you are {0}. Nice to meet you, My name's {1}. Our both names start with 's' lmao!! ".format(name,S_name_c))
+
+elif name[0] == T_name[0]:
+    print(" wow seriously, you are {0}. Nice to meet you, My name's {1}. Our both names start with 't' lmao!! ".format(name,T_name_c))
+
+elif name[0] == U_name[0]:
+    print(" wow seriously, you are {0}. Nice to meet you, My name's {1}. Our both names start with 'u' lmao!! ".format(name,U_name_c))
+
+elif name[0] == V_name[0]:
+    print(" wow seriously, you are {0}. Nice to meet you, My name's {1}. Our both names start with 'v' lmao!! ".format(name,V_name_c))
+
+elif name[0] == W_name[0]:
+    print(" wow seriously, you are {0}. Nice to meet you, My name's {1}. Our both names start with 'w' lmao!! ".format(name,W_name_c))
+
+elif name[0] == X_name[0]:
+    print(" wow seriously, you are {0}. Nice to meet you, My name's {1}. Our both names start with 'x' lmao!! ".format(name,X_name_c))
+
+elif name[0] == Y_name[0]:
+    print(" wow seriously, you are {0}. Nice to meet you, My name's {1}. Our both names start with 'y' lmao!! ".format(name,Y_name_c))
+
+elif name[0] == Z_name[0]:
+    print(" WOW seriously, you are {0}. Nice to meet you, My name's {1}. Our both names start with 'z' lmao!! ".format(name,Z_name_c))
+
+print('*' *140)
+
+# MAIN MENU
+s1 = int(input("""Please select one of the following:
+
+1] Delivery issue
+2] Packaging issue
+3] Refund issue
+4] Account/profile related issues
+5] Feedback
+
+Your answer here: """))
+
+# NO-1 DELIVERY ISSUES
+if s1 == 1:
+    s1_1 = int(input("""Please let us know better about the delivery issue:
+
+1] My delivery is late than usual
+2] My delivery is not here yet!
+3] Delivery person not responding
+4] Not satisfied with the delivery 
+5] Take me back to the main menu
+
+Your answer here: """))
+
+    if s1_1 == 1:
+        print(""" We are texting you the delivery agents contact details, 
+        and we are arranging a call back from our side
+        have a nice day {0}""".format(name))
+
+    elif s1_1 == 2:
+        print(""" The delivery agent is on the way, traffic is always unpredictable.
+        we request you to wait for somr time before taking any action""")
+
+    elif s1_1 == 3:
+        print(""" We are texting you the delivery agents contact details, 
+        and we are arranging a call back from our side
+        have a nice day {0}""".format(name))
+    elif s1_1 == 4:
+        s1_1_1 = int(input(""" we are very sorry for your inconvinience {0}.
+        On a scale from 1-5, let us know how bad was the delivery""".format(name)))
+        if s1_1_1 < 4:
+            print(""" We are extremely sorry, you will get a full refund + a zwiggy voucher in your account,
+            have a nice day {0}""".format(name))
+        else:
+            print(""" We understand the delivery was a bit dissapointing, We will try and improve our standards,
+             have a nice day {0}""".format(name))
+    elif s1_1 == 5:
+        print(""" This chat ends here, have a nice day {0}""".format(name))
+
+# NO-2 PACKAGING ISSUES
+elif s1 == 2:
+    s1_2 = int(input("""Please let us know better about the packaging issue:
+    
+    1] Not packed properly
+    2] Package too loose
+    3] Contact the restaurant
+    """))
+    if s1_2 == 1:
+        s1_2_1 = int(input(""" we are very sorry for your inconvinience {0}.
+        On a scale from 1-5, let us know how bad was the packaging""".format(name)))
+        if s1_2_1 < 4:
+            print(""" We are extremely sorry, you will get a full refund + a zwiggy voucher in your account,
+            have a nice day {0}""".format(name))
+        else:
+            print(""" We understand the packaging was a bit unsatisfactory, We will try and improve our standards,
+             have a nice day {0}""".format(name))
+    elif s1_2 == 2:
+        s1_2_2 = int(input("""we are very sorry for your inconvinience {0}.
+        On a scale from 1-5, let us know how bad was the packaging""".format(name)))
+        if s1_2_2 < 4:
+            print(""" We are extremely sorry, you will get a full refund + a zwiggy voucher in your account,
+            have a nice day {0}""".format(name))
+        else:
+            print(""" We understand the packaging was a bit unsatisfactory, We will try and improve our standards,
+             have a nice day {0}""".format(name))
+
+    elif s1_2 ==3:
+        s1_2_1 = input("{0},Please enter your new mobile number here :".format(name))
+        print(" ")
+        print("We have sent the restaurant details to your provided mobile mr.{0},".format(name))
+
+
+# NO-3 REFUND ISSUES
+elif s1 == 3:
+    s1_3 = int(input("""Please let us know more about your refund:
+    1] Apply for a refund
+    2] Status of the refund
+    3] Refund policy
+    """))
+    if s1_3 == 1:
+        s1_3_1 = int(input(""" ah! i understand the food was not upto the standards,
+        We are extremely sorry, on a scale from 1-5 how bad was the food"""))
+        if s1_3_1 < 4:
+            print(""" We are extremely sorry, you will get a full refund + a zwiggy voucher in your account,
+            have a nice day {0}""".format(name))
+        else:
+            print(""" We understand the food was a bit unsatisfactory, We will try and improve our standards,
+             have a nice day {0}""".format(name))
+
+    elif s1_3 ==2:
+        s1_3_2 = int(input(""" Please enter your mobile number : """))
+        print(""" Hello {0}, your refund is in process,
+        the money will reflect in your bank account in 3-4 business days""".format(name))
+    elif s1_3 ==3:
+        print(""" please visit :
+        www.zwiggy.cum/refunds/refundpolicy 
+        for more details.""")
+
+# NO-4 ACCOUNT RELATED ISSUES
+elif s1 == 4:
+    s1_4 = int(input("""What changed would you like to make to your account:
+    1] Change the username 
+    2] Change email adress
+    3] Verify Contact details
+    4] trouble with the location
+    """))
+    if s1_4 == 1:
+        s1_4_1 = input("{0},Please enter your new username here :".format(name))
+        print(" ")
+        print("Congratulations {0}, Your new user name '{1}' has been updated".format(name,s1_4_1))
+
+    elif s1_4 == 2:
+        s1_4_2 = input("{0},Please enter your new email address here :".format(name))
+        print(" ")
+        print("We have sent a verification link to your mail {0}, Please confirm".format(name))
+
+    elif s1_4 == 3:
+        s1_4_3 = input("{0},Please enter your new mobile number here :".format(name))
+        print(" ")
+        print("We have sent an OTP to your mobile mr.{0}, Please confirm".format(name))
+
+    elif s1_4 == 4:
+        print("""The GPS ( location service) may malfunction if you are using a proxy network,
+        I sincerely request you to turn any kind of proxy settings before trying again.
+        
+         If the issue still persists, You are welcome to contact us 24/7
+         thanks{0}, Have a nice day :)""".format(name))
+
+
+
+
+# NO-5 FEEDBACK
+elif s1 == 5:
+    s1_5 = int(input("""Your feedback is valuable to us,
+    On a scale from 1-10
+    1 being the worst
+    10 being the best
+    How would you rate us:
+    """))
+    if s1_5 > 5:
+        print(""" Wow, We are glad that you liked zwiggy, Nothing makes us more happy than a happy customer,
+              Thanks have a nice day :) """)
+
+    elif s1_5 == 5:
+        print(""" dang it, we are half way there """)
+
+    else:
+        print(""" We will definitely try to improve and your feedback means a lot, thanks for being honest""")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
